b14c0504ac47123dc5c1692bf25f5824dfbedee3
Mason Jenkins <mason.jenkins00@gmail.com>
2022-12-07 00:06:05 -0600

    modal, sliding items, list

--- budget/src/app/tab1/tab1.page.ts
+++ budget/src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core';
+
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,6 +10,35 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  @ViewChild(IonModal) modal!: IonModal;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name!: string;
+  date!: Date;
+  amount!: Number;
+  type!: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+    console.log(this.name)
+    console.log(this.date)
+    console.log(this.amount)
+    console.log(this.type)
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
+
+
 
   constructor() {}
 
